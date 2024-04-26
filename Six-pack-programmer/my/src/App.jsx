a3dd2932226aaa4bc12636cf6b95bf555e76c11b
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-26 16:45:42 +0530

    updater

---
+++ Six-pack-programmer/my/src/App.jsx
@@ -0,0 +1,22 @@
+import Myheading,{MyheadingLittle,MyheadingLittle3,MyheadingLittle2,MyheadingLittle4} from "./Components/Myheading";
+
+
+function App(){
+  return(
+    <>
+    <div>
+      <Myheading/>  
+      <MyheadingLittle2/>      
+      <MyheadingLittle3/>      
+      <MyheadingLittle4/>      
+      <h1>
+        Gyanranjan Patra
+      </h1>
+    </div>
+    
+    </>
+  )
+}
+
+
+export default App;
---
+++ Six-pack-programmer/my/src/Components/Myheading.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Myheading() {
+
+    const a =69;
+    return (
+
+        <>
+            <h1>
+                My Heading{a}
+            </h1>
+        </>
+    )
+}
+const MyheadingLittle=()=> <h1>Gyanranjan   Patra</h1>
+const MyheadingLittle2=()=> <h1>Heading 2</h1>
+const MyheadingLittle3=()=> <h1>Gyanranjan   Patra3</h1>
+const MyheadingLittle4=()=> <h1>Gyanranjan   Patra4</h1>
+
+export default Myheading
+export{MyheadingLittle,MyheadingLittle2,MyheadingLittle3,MyheadingLittle4}
