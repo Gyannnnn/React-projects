4586382328bc3ddd8c5086c443bbff3ed28c8f04
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-27 11:10:05 +0530

    3

---
+++ React Practice-6pp/6Pack-Programmer/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Components/Header"
+import Body from "./Components/Body"
+import "./styles/App.css"
+import "./styles/Header.css"
+import "./styles/Body.css"
+
+function App(){
+  return(
+    <>
+     <Header/>
+     <Body></Body>
+
+    </>
+  )
+}
+
+
+export default App;
---
+++ React Practice-6pp/6Pack-Programmer/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+        NavBar
+    </nav>
+  )
+}
+
+export default Header
