d13f8ac0d50dbad5bf0ef09caed2a75f9edf7794
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-28 11:27:08 +0530

    5

--- React Practice-6pp/6Pack-Programmer/src/App.jsx
+++ React Practice-6pp/6Pack-Programmer/src/App.jsx
@@ -1,17 +1,24 @@
 import Header from "./Components/Header"
-import Body from "./Components/Body"
 import "./styles/App.css"
 import "./styles/Header.css"
-import "./styles/Body.css"
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
 function App(){
-  return(
-    <>
-     <Header/>
-     <Body></Body>
+  return <Router>
 
-    </>
-  )
+
+    <Header/>
+
+    <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="*" element = {<div><h1>Page Not Found 404</h1></div>}/>
+    </Routes>
+
+  </Router>
 }
 
 
--- React Practice-6pp/6Pack-Programmer/src/Components/Header.jsx
+++ React Practice-6pp/6Pack-Programmer/src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import {Link} from "react-router-dom"
+
 function Header() {
   return (
     <nav>
-        NavBar
+        <Link to="/Home">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
     </nav>
   )
 }
